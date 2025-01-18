7c8e0bd3c986b694f649cdec7609915d06a3a3c8
atrox3d <atrox3d@gmail.com>
2025-01-18 10:57:21 +0100

    add Annotated type

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import typer
+from typing import Annotated
 
 app = typer.Typer()
 
+USERSLIST_TYPE = Annotated[list[str], typer.Argument(help='List of users')]
+VERBOSE_TYPE = Annotated[bool, typer.Option(help='Show verbose output')]
+
 @app.command()
-def add_users(users:list[str], verbose:bool=False):
+def add_users(users:USERSLIST_TYPE, verbose:VERBOSE_TYPE=False):
     '''add users to the current users db'''
     for user in users:
         if verbose:
