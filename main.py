eacf08fa7d2c09e384b508bbd7eda5501b0b1152
atrox3d <atrox3d@gmail.com>
2025-01-18 11:02:58 +0100

    implement delete users

--- main.py
+++ main.py
@@ -3,20 +3,40 @@ from typing import Annotated
 
 app = typer.Typer()
 
+active_users = [
+    'rob',
+    'fab',
+    'john'
+]
+
 USERSLIST_TYPE = Annotated[list[str], typer.Argument(help='List of users')]
 VERBOSE_TYPE = Annotated[bool, typer.Option(help='Show verbose output')]
 
 @app.command()
-def add_users(users:USERSLIST_TYPE, verbose:VERBOSE_TYPE=False):
+def add_users(
+    users:USERSLIST_TYPE, 
+    verbose:VERBOSE_TYPE=False
+):
     '''add users to the current users db'''
     for user in users:
         if verbose:
             print(f'user {user} added')
     print('add complete')
 
+
 @app.command()
-def delete_users():
-    pass
+def delete_users(
+    users:USERSLIST_TYPE, 
+    verbose:VERBOSE_TYPE=False
+):
+    '''delete users from active db'''
+    for user in users:
+        if user not in active_users:
+            print(f'user {user} not in active db')
+        else:
+            if verbose:
+                print(f'user {user} deleted')
+    print('deletion complete')
 
 
 @app.command()
