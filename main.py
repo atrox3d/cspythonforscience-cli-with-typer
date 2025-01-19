a1eb65b3a984e4d87c023f2f4593f0fe5e2ce444
atrox3d <atrox3d@gmail.com>
2025-01-19 08:32:55 +0100

    add authenticated decorator

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # https://www.youtube.com/watch?v=gOQCFPSjJ-8
 
+from functools import wraps
 import typer
 from typing import Annotated
 import rich
@@ -12,12 +13,26 @@ active_users = [
     'john'
 ]
 
+admin_credentials = {'username': 'admin', 'password': 'password'}
+
+def authenticated(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        auth = {k:v for k, v in kwargs.items() if k in 'username password'.split()}
+        if auth != admin_credentials:
+            rich.print('[bold red]:locked_with_key: invalid credentials[/bold red]')
+            exit(1)
+        return func(*args, **kwargs)
+    return wrapper
+
+
 USERSLIST_TYPE = Annotated[list[str], typer.Argument(help='List of users')]
 VERBOSE_TYPE = Annotated[bool, typer.Option(help='Show verbose output')]
 USERNAME_TYPE = Annotated[str, typer.Option(help='your username')]
 PASSWORD_TYPE = Annotated[str, typer.Option(help='your password', prompt=True, hide_input=True)]
 
 @app.command()
+@authenticated
 def add_users(
     users:USERSLIST_TYPE,
     password:PASSWORD_TYPE,
